bae109c2540da3bffc3342b5713c8d53560e990b
DimaBLR <76612985+DimaBLR@users.noreply.github.com>
2021-02-16 09:24:40 +0300

    docs: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+    if (arguments.length == 0 || arr.length == 0) {
+        return 0
+    }
+    arr.sort((a, b) => a - b)
+    return arr[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+    if (arguments.length == 0 || arr.length == 0) {
+        return 0
+    }
+    arr.sort((a, b) => b - a)
+    return arr[0];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+    if (arguments.length == 0 || arr.length == 0) {
+        return 0
+    }
+    let result = arr.reduce((a, b) => a + b)
+    return result / arr.length
 }
